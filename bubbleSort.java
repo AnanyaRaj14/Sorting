397f4227e51a6c5a06a9474120d62b8502b6f618
Ananya Raj <ananyam748@gmail.com>
2024-08-30 07:45:35 +0530

    first commit

---
+++ bubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class bubbleSort{
+    public static void main(String[] args){
+        int[] arr = {7, 6, 5, 4, 3, 2};
+        bubbleSort(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+
+    static void bubbleSort(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp =  arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
